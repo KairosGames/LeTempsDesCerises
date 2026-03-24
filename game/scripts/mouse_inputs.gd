da8296eed5adec229b7df7f43ef9c6fb5d65899a
BENMANSOUR Ydris <ydris.benmansour.auditeur@lecnam.net>
2026-03-24 15:20:27 +0100

    WIP-aim-controller

--- game/scripts/mouse_inputs.gd
+++
@@ -1,8 +0,0 @@
-class_name MouseInputs extends Node
-
-@export_category("Settings")
-@export_range(0.1, 10.0, 0.1) var sensitivity: float = 1.0
-@export var is_inverted: bool
-
-var yaw: float
-var pitch: float
