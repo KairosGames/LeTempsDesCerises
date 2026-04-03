87e9cc077a54dcf8dc48e998321a76bd5393bab3
Ahzzak <barthelemivincent@gmail.com>
2026-04-03 16:59:57 +0200

    Wwise project setup + gun placeholders / cannons ambience

--- game/sounds/banks/Wwise_IDs.h
+++ game/sounds/banks/Wwise_IDs.h
@@ -11,10 +11,85 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID AMB_CANNONS = 3265814676U;
+        static const AkUniqueID PLAYER_RELOAD = 1650679582U;
+        static const AkUniqueID PLAYER_SHOOT = 4004702906U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PLAYER_AIM
+        {
+            static const AkUniqueID GROUP = 1608601952U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FALSE = 2452206122U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TRUE = 3053630529U;
+            } // namespace STATE
+        } // namespace PLAYER_AIM
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace ALLEGIANCE
+        {
+            static const AkUniqueID GROUP = 1922831386U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID COMMUNARD = 149546059U;
+                static const AkUniqueID VERSAILLAIS = 2495691590U;
+            } // namespace SWITCH
+        } // namespace ALLEGIANCE
+
+        namespace CHARACTER_TYPE
+        {
+            static const AkUniqueID GROUP = 1117085073U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID TYPE1 = 408582742U;
+                static const AkUniqueID TYPE2 = 408582741U;
+            } // namespace SWITCH
+        } // namespace CHARACTER_TYPE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID CANNONS_PROBABILITY = 2576556105U;
+        static const AkUniqueID POETIC_LEVEL = 3680281974U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SB_PLAYER = 2103316850U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
+        static const AkUniqueID AMB = 1117531639U;
         static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID SFX_NPC = 161171466U;
+        static const AkUniqueID SFX_PLAYER = 217780010U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID VX = 1534528563U;
+        static const AkUniqueID VX_NPC = 1277101407U;
+        static const AkUniqueID VX_PLAYER = 1728828729U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID CROSSROAD_REVERB = 1865335600U;
+        static const AkUniqueID STREET_REVERB = 2635253023U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
