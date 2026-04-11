f747d21fa2503e0f90933fefc3b2cd8b6ed2c917
Ydris <ydris.benmansour@studioblackflag.com>
2026-04-11 18:37:21 +0200

    WIP-polish-stateswitch

--- game/prefabs/player/player.gd
+++ game/prefabs/player/player.gd
@@ -22,7 +22,9 @@ class_name Player extends CharacterBody3D
 
 @export_category("View settings")
 @export var v_clamp_deg: Vector2 = Vector2(-70.0, 70.0)
-@export_range(0.01, 1.0, 0.01) var aiming_reducer_ratio: float = 0.5
+@export var v_clamp_lyingd: Vector2 = Vector2(-45.0, 45.0)
+@export_range(0.01, 1.0, 0.01) var aiming_reducer_ratio: float = 0.4
+@export_range(0.1, 1.0, 0.01) var lyingd_reducer_ratio: float = 0.3
 
 @export_category("Aim settings")
 @export var default_fov: float = 75.0
@@ -32,9 +34,6 @@ class_name Player extends CharacterBody3D
 @export var aiming_rot: float = 1.0
 
 @export_category("States settings")
-@export var can_fast_down: bool = true
-@export var can_run_crouched: bool = true
-@export var can_run_lyied_d: bool = true
 @export var standing_height: float = 1.6
 @export var crouch_height: float = 1.1
 @export var lyingd_height: float = 0.35
@@ -44,7 +43,7 @@ class_name Player extends CharacterBody3D
 @export var standing_speed: float = 5.0
 @export var crouch_speed: float = 3.0
 @export var lying_d_speed: float = 1.0
-@export var air_up_speed: float = 0.1
+@export_range(0.0, 1.0, 0.01) var air_up_speed: float = 1.0
 @export_range(1.0, 3.0, 0.01) var run_speed_ratio: float = 1.5
 @export_range(0.0, 1.0, 0.01) var side_speed_ratio: float = 0.75
 @export_range(0.0, 1.0, 0.01) var back_speed_ratio: float = 0.6
@@ -66,14 +65,16 @@ var is_grounded: bool = true
 var is_aiming: bool = false
 var is_running: bool = false
 var is_crouched:bool = false
-var is_lyied_d: bool = false
+var is_lyingd: bool = false
 var is_changing_state: bool = false
+var stop_run: bool = false
 
 var wpn_x_aim_twn: Tween
 var wpn_y_aim_twn: Tween
 var wpn_z_aim_twn: Tween
 var fov_aim_twn: Tween
 var cam_rot_aim_twn: Tween
+var state_twn: Tween
 
 var aim_vel: Vector3 = Vector3.ZERO
 var aim_target: Vector3 = Vector3.ZERO
@@ -91,13 +92,13 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	capture_states()
-	handle_states(delta)
 	process_movement(delta)
 	process_view(delta)
 	handle_shoot(delta)
 
 
 func capture_states() -> void:
+	is_grounded = is_on_floor()
 	capture_aim_state()
 	capture_position_state()
 
@@ -105,10 +106,13 @@ func capture_states() -> void:
 func capture_aim_state() -> void:
 	var flag: bool = is_aiming
 	if is_aim_locked:
-		if Input.is_action_just_pressed("aim"): is_aiming = !is_aiming
+		if Input.is_action_just_pressed("aim"):
+			is_aiming = !is_aiming
+			is_running = false
 	else:
 		is_aiming = Input.is_action_pressed("aim")
-	if Input.is_action_just_pressed("run") and is_aiming:
+		if is_aiming: is_running = false
+	if ((Input.is_action_just_pressed("run") and not stop_run) or Input.is_action_just_pressed("jump")) and is_aiming:
 		is_aiming = false 
 	player_inputs.is_aiming = is_aiming
 	if flag != is_aiming :
@@ -146,55 +150,85 @@ func switch_aim_state() -> void:
 
 func capture_position_state() -> void:
 	var input_dir: Vector2 = player_inputs.move_vec
+	stop_run = input_dir.y <= 0.0 or abs(input_dir.x) > 0.71 or input_dir.length() < gpad_mini_run_length
+	if stop_run: is_running = false
+	
+	if not is_grounded: return
 	
 	if Input.is_action_just_pressed("crouch"):
-		if not is_grounded: return
-		if is_lyied_d: is_lyied_d = false
-		if is_running: is_running = false
-		is_crouched = !is_crouched
+		if is_crouched: crouch_to_up()
+		elif is_lyingd: lyingd_to_crouch()
+		else: crouch_to_up(true)
 	
 	if Input.is_action_just_pressed("lying_down"):
-		if not is_grounded: return
-		if is_lyied_d: is_crouched = true
-		if is_running: is_running = false
-		is_lyied_d = !is_lyied_d
-		
-	if Input.is_action_just_pressed("run"):
-		if not is_grounded: return
-		if is_crouched: is_crouched = false
-		if is_lyied_d: is_lyied_d = false
-		if is_aiming: is_aiming = false
-		is_running = !is_running
+		if is_crouched: lyingd_to_crouch(true)
+		elif is_lyingd: lyingd_to_up()
+		else: lyingd_to_up(true)
 	
-	if input_dir.y <= 0.0 or abs(input_dir.x) > 0.71 or input_dir.length() < gpad_mini_run_length:
-		is_running = false
-		pass
+	if Input.is_action_just_pressed("run") and not stop_run:
+		if is_crouched: crouch_to_up()
+		elif is_lyingd: lyingd_to_up()
+		else: is_running = true
+	
+	if Input.is_action_just_pressed("jump"):
+		if is_crouched: crouch_to_up()
+		elif is_lyingd: lyingd_to_up()
+		else: jump()
 
 
-func handle_states(delta: float) -> void:
-	handle_position_state(delta)
+func crouch_to_up(inverse: bool = false, ask_run: bool = false):
+	is_changing_state = true
+	if inverse: is_running = false
+	if ask_run: is_running = true
+	var target: float = crouch_height if inverse else standing_height
+	state_twn = create_tween()
+	state_twn.tween_property(camera_pivot, "position:y", target, 0.2)
+	await state_twn.finished
+	is_crouched = inverse
+	is_changing_state = false
 
 
-func handle_position_state(delta: float) -> void:
-	if not is_crouched and not is_lyied_d: camera_pivot.position.y = 1.6
-	if is_crouched : camera_pivot.position.y = 1.1
-	if is_lyied_d: camera_pivot.position.y = 0.35
+func lyingd_to_crouch(inverse: bool = false):
+	is_changing_state = true
+	var target: float = lyingd_height if inverse else crouch_height
+	state_twn = create_tween()
+	state_twn.tween_property(camera_pivot, "position:y", target, 0.2)
+	await state_twn.finished
+	is_crouched = not inverse
+	is_lyingd = inverse
+	is_changing_state = false
+
+
+func lyingd_to_up(inverse: bool = false, ask_run: bool = false):
+	is_changing_state = true
+	var target: float = lyingd_height if inverse else standing_height
+	state_twn = create_tween()
+	state_twn.tween_property(camera_pivot, "position:y", crouch_height, 0.2)
+	await state_twn.finished
+	await get_tree().create_timer(0.1).timeout
+	state_twn = create_tween()
+	state_twn.tween_property(camera_pivot, "position:y", target, 0.2)
+	await state_twn.finished
+	camera_pivot.position.y = lyingd_height if inverse else standing_height
+	is_lyingd = inverse
+	if ask_run: is_running = true
+	is_changing_state = false
+
+
+func jump():
+	velocity.y = JUMP_VELOCITY
 
 
 func process_movement(delta: float) -> void:
 	apply_plane_movement(delta)
-	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
 	move_and_slide()
 
 
 func apply_plane_movement(delta: float) -> void:
 	var mov_vec: Vector2 = player_inputs.move_vec
+	var dir: Vector3 = (transform.basis * Vector3(mov_vec.x, 0, mov_vec.y).normalized())
 	var ref_speed = get_used_speed()
 	var run_f: float = run_speed_ratio if is_running else 1.0
 	var aim_f: float = aiming_speed_ratio if is_aiming else 1.0
@@ -204,6 +238,10 @@ func apply_plane_movement(delta: float) -> void:
 	var s_speed: float = abs(mov_vec.x) * ref_speed * side_speed_ratio
 	var dir_speed: float = sqrt(f_speed * f_speed + s_speed * s_speed)
 	var applied_speed: float = dir_speed * run_f * aim_f
+	
+	if not is_grounded:
+		velocity += dir * ref_speed * delta
+		return
 	
 	if is_movement_smooth:
 		var lcl_vel: Vector3 = global_transform.basis.inverse() * velocity
@@ -228,18 +266,17 @@ func apply_plane_movement(delta: float) -> void:
 		
 		velocity = global_transform.basis * lcl_vel
 	else:
-		var dir: Vector3 = (transform.basis * Vector3(mov_vec.x, 0, mov_vec.y).normalized())
 		var speed: Vector3 = Vector3(dir.x * applied_speed, 0.0, dir.z * applied_speed)
 		velocity = Vector3(speed.x, velocity.y, speed.z) if dir else Vector3(0.0, velocity.y, 0.0)
 
 
 func get_used_speed() -> float:
-	if is_crouched:
-		return crouch_speed
-	elif is_lyied_d:
-		return lying_d_speed
-	elif not is_grounded:
+	if not is_grounded:
 		return air_up_speed
+	elif is_crouched:
+		return crouch_speed
+	elif is_lyingd:
+		return lying_d_speed
 	else:
 		return standing_speed
 
@@ -247,11 +284,12 @@ func get_used_speed() -> float:
 func process_view(delta: float) -> void:
 	if not is_mouse_locked(): return
 	var input: PlayerInputs = player_inputs
-	var inversion = -1 if player_inputs.is_inverted else 1
-	var reducer = aiming_reducer_ratio if is_aiming else 1.0
+	var inversion: float = -1 if player_inputs.is_inverted else 1
+	var reducer: float = (aiming_reducer_ratio if is_aiming else 1.0) * (lyingd_reducer_ratio if is_lyingd else 1.0)
 	aim_target.y -= input.get_view_input().x * input.h_sensi_multiplier * reducer
 	aim_target.x += input.get_view_input().y * input.v_sensi_multiplier * inversion * reducer
-	aim_target.x = clampf(aim_target.x, v_clamp_deg.x, v_clamp_deg.y)
+	var clamp_applied: Vector2 = v_clamp_lyingd if is_lyingd else v_clamp_deg
+	aim_target.x = clampf(aim_target.x, clamp_applied.x, clamp_applied.y)
 	
 	if is_aim_smooth:
 		var result_y: Dictionary = smooth_damp_angle(rotation_degrees.y, aim_target.y, aim_vel.y, aim_smooth_strength, delta)
