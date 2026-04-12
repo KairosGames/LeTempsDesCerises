98dc6db5008278d77681eea73d293895323093a5
D4r3N <enzo.suares@icloud.com>
2026-04-12 14:54:41 +0200

    increase ai stop distance

--- game/sandbox/ai/behaviours/move_to_cover.gd
+++ game/sandbox/ai/behaviours/move_to_cover.gd
@@ -2,7 +2,7 @@
 class_name MoveToCover extends MoveAction
 
 func get_stop_distance() -> float:
-	return 0.2
+	return 1
 
 func get_destination(_actor: Node, blackboard: Blackboard) -> Vector3: 
 	return blackboard.get_value("cover").global_position
