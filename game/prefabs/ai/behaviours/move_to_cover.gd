6fec3628990c78cb5abf3db97e065687cb091cf0
D4r3N <enzo.suares@icloud.com>
2026-04-13 15:57:42 +0200

    fix: agent get cover on cover reached

--- game/prefabs/ai/behaviours/move_to_cover.gd
+++ game/prefabs/ai/behaviours/move_to_cover.gd
@@ -11,15 +11,11 @@ func on_start(actor: Node, blackboard: Blackboard) -> int:
 	var cover: Cover = blackboard.get_value("cover")
 	if not cover or not CoverManager.try_take_cover(cover):
 		return FAILURE
-	else:
-		(actor as Agent).cover = cover
+	else: 
 		return RUNNING
 
-# func has_succeeded(actor: Node, blackboard: Blackboard) -> int:
-# 	var cover: Cover = blackboard.get_value("cover")
-# 	if CoverManager.try_take_cover(cover):
-# 		var agent: Agent = actor
-# 		agent.cover = cover
-# 		return SUCCESS
-# 	else:
-# 		return FAILURE
+func has_succeeded(actor: Node, blackboard: Blackboard) -> int:
+	var cover: Cover = blackboard.get_value("cover")
+	var agent: Agent = actor
+	agent.cover = cover
+	return SUCCESS
