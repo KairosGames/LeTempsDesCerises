994f4d592b2f81beafae81b181b9dd2831a371f2
D4r3N <enzo.suares@icloud.com>
2026-04-24 14:38:07 +0200

    Fix missing Canon.singleton

--- game/prefabs/ai/behaviours/conditions/is_canon_available.gd
+++ game/prefabs/ai/behaviours/conditions/is_canon_available.gd
@@ -2,6 +2,7 @@
 class_name IsCanonAvailable extends ConditionLeaf
 
 func tick(_actor: Node, blackboard: Blackboard) -> int:
+	if not Canon.singleton: return FAILURE
 	for slot in Canon.singleton.slots:
 		if not slot.get_child_count():
 			blackboard.set_value("canon_slot", slot)
