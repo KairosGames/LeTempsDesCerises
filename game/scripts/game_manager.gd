cdfad32f2a41508bf196f2cec2ddabf36a263a6e
Ydris <ydris.benmansour@studioblackflag.com>
2026-05-09 19:05:40 +0200

    add-barricade-in-game-manager

--- game/scripts/game_manager.gd
+++ game/scripts/game_manager.gd
@@ -2,5 +2,13 @@ class_name GameManager extends Node
 
 static var active_fight_area: FightArea
 
-@export_category("Export settings")
+@export_category("References")
 @export var player: Player
+@export var first_barricade: Barricade
+
+var step: int = 0
+
+
+func get_active_barricade() -> Barricade:
+	if step == 0: return first_barricade
+	return null
