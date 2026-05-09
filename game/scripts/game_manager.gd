501566718277c04b93fa1888ab88dac436e0244d
Ydris <ydris.benmansour@studioblackflag.com>
2026-05-09 20:07:25 +0200

    fixes

--- game/scripts/game_manager.gd
+++ game/scripts/game_manager.gd
@@ -5,8 +5,18 @@ static var active_fight_area: FightArea
 @export_category("References")
 @export var player: Player
 @export var first_barricade: Barricade
+@export var canon: Canon
 
 var step: int = 0
+
+
+func _ready() -> void:
+	if canon: canon.shoot.connect(handle_canon_shoot)
+
+
+func handle_canon_shoot() -> void:
+	var curr_barricade = get_active_barricade()
+	if curr_barricade: curr_barricade.take_damage()
 
 
 func get_active_barricade() -> Barricade:
