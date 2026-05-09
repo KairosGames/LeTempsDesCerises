a2c474914ecdbe8b9285dbc7d650d1dd098d5011
D4r3N <enzo.suares@icloud.com>
2026-05-09 19:02:17 +0200

    random next_cover

--- game/prefabs/ai/behaviours/actions/find_next_cover.gd
+++ game/prefabs/ai/behaviours/actions/find_next_cover.gd
@@ -4,9 +4,18 @@ class_name FindNextCover extends ActionLeaf
 func tick(actor: Node, _blackboard: Blackboard) -> int:
 	var agent: Agent = actor
 	var current_cover: Cover = agent.cover
+
 	if not current_cover: return FAILURE
-	for next_cover: Cover in current_cover.next_covers:
-		if next_cover.enabled and not next_cover.holder:
-			agent.cover = next_cover
-			return SUCCESS
-	return FAILURE
+
+	var covers: Array[Cover] = current_cover.next_covers.duplicate()
+
+	for i in range(covers.size() -1, -1, -1):
+		var cover: Cover = covers[i]
+		if not cover.enabled or cover.holder: covers.remove_at(i)
+
+	if not covers.size(): return FAILURE
+
+	var next_cover: Cover = covers.pick_random()
+
+	agent.cover = next_cover
+	return SUCCESS
