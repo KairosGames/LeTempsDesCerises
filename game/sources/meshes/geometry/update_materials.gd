6f136509fac679253387a263a153ef7a4e51ea4c
D4r3N <enzo.suares@icloud.com>
2026-05-21 19:12:47 +0200

    new shop and bite

--- game/sources/meshes/geometry/update_materials.gd
+++ game/sources/meshes/geometry/update_materials.gd
@@ -24,5 +24,7 @@ func _run() -> void:
 			var surface_name: String = mesh.surface_get_name(index)
 			if materials.has(surface_name):
 				mesh.surface_set_material(index, materials[surface_name])
+			else:
+				push_warning("material ",surface_name," not available")
 		var output_path: String = OUTPUT_DIR + name + ".tres"
 		ResourceSaver.save(mesh, output_path)
