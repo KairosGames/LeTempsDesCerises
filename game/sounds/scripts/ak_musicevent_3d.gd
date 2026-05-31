24b2cdac80764e46bf40506c6b291766a38c3a55
Xelonn <124406589+Xelonn@users.noreply.github.com>
2026-06-01 00:08:31 +0200

    3D Music systeme
    Systeme Musique via un prefab

---
+++ game/sounds/scripts/ak_musicevent_3d.gd
@@ -0,0 +1,27 @@
+extends AkEvent3D
+
+@export var Voice_sel: String = "None"
+@export var Distance_scale: int = 20
+@export var DistScale:WwiseRTPC
+@export var IsPlay:WwiseRTPC
+@export var Is_playing: float = 0
+@export var Enter_at: String = "None"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Wwise.set_switch("Voice_Sel", Voice_sel, self)
+	DistScale.set_value(self, Distance_scale)
+	Wwise.set_state("Music_State", "Phase1")
+	post_event()
+	
+	IsPlay.set_value(self, Is_playing)
+
+func _on_music_sync_user_cue(data: Dictionary) -> void:
+	
+	if data.has("pszUserCueName"):
+		var user_cue_name: String = data["pszUserCueName"]
+		
+		if user_cue_name == Enter_at: # Rendre changeable dans le préfab
+			Is_playing = 100.0
+			IsPlay.set_value(self, Is_playing)
+			print("j'ai trouvé : ", user_cue_name)
