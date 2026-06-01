6d3ffb0dff0fa443d4163e4f48d33e4183d725ab
Ydris <ydris.benmansour@studioblackflag.com>
2026-06-02 00:36:14 +0200

    WIP

--- game/prefabs/game_states/game_state_01.gd
+++ game/prefabs/game_states/game_state_01.gd
@@ -1,10 +1,18 @@
 class_name GameState01 extends GameState
 
+@onready var go_to_barricade_2: EventArea = %GoToBarricade2
+@onready var return_to_barricade: EventArea = %ReturnToBarricade
+
 
 func enter() -> void:
 	curr_step = 0
-	steps = []
+	steps = [
+		Step.new(do_nothing, do_nothing, do_nothing),
+		Step.new(do_nothing, do_nothing, do_nothing),
+		Step.new(do_nothing, do_nothing, do_nothing),
+	]
 	run_steps()
+
 
 func exit() -> void:
 	pass
