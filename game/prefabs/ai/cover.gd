434dec671b48f1bf9e6ffcd9041e7a9a8706c31e
D4r3N <enzo.suares@icloud.com>
2026-06-05 10:12:16 +0200

    really fix cover

--- game/prefabs/ai/cover.gd
+++ game/prefabs/ai/cover.gd
@@ -187,7 +187,7 @@ func _update_motions() -> void:
 
 	for i: int in range(next_covers.size()):
 		var next_cover: Cover = next_covers[i]
-		if is_instance_valid(next_cover):
+		if not is_instance_valid(next_cover):
 			_motions.multimesh.set_instance_transform(i, Transform3D(Basis.from_scale(Vector3.ZERO), Vector3.ZERO))
 			break
 		var color: Color = colors[Type.TRANSITORY] if next_cover.type == Type.TRANSITORY else colors[type]
