429e05cdd734ad9e4ba74942f738209d9a65a284
D4r3N <enzo.suares@icloud.com>
2026-06-06 00:18:15 +0200

    move shoot_debug line fading in shader

--- game/sandbox/ai/shoot_debug.gd
+++ game/sandbox/ai/shoot_debug.gd
@@ -6,36 +6,32 @@ const POOL_SIZE: int = 100
 const DEBUG_LINE: BoxMesh = preload("uid://dlk3762ys1jgb")
 
 var _open: Array[int] = []
-var _close: Array[int] = []
 
 func _ready() -> void:
 	instance = self
 	multimesh = MultiMesh.new()
 	multimesh.use_colors = true
+	multimesh.use_custom_data = true
+	multimesh.mesh = DEBUG_LINE
 	multimesh.transform_format = MultiMesh.TRANSFORM_3D
 	multimesh.instance_count = POOL_SIZE
-	multimesh.mesh = DEBUG_LINE
-	for i: int in range(POOL_SIZE): 
+	for i: int in range(POOL_SIZE):
 		_open.append(i)
-		multimesh.set_instance_color(i, Color.RED)
-		multimesh.set_instance_transform(i, Transform3D.IDENTITY)
 
-func add_debug(origin: Vector3, direction: Vector3, color: Color, is_player: bool, duration: float = 10.0, distance: float = 100.0) -> void:
+func add_debug(origin: Vector3, direction: Vector3, color: Color, is_player: bool, duration: float = 3.0, distance: float = 100.0) -> void:
 	if not _open.size(): printerr("pool is not big enough"); return
-	
+
 	var index: int = _open.pop_front()
-	_close.append(index)
-	
+
 	var line_basis: Basis = Basis.from_euler(direction)
 	multimesh.set_instance_transform(index, Transform3D(line_basis, origin + (1 if is_player else -1) * line_basis.z * distance / 2.0 ))
-	multimesh.set_instance_color(index, color)
+	multimesh.set_instance_color(index, Color(color, 1.0))
+	multimesh.set_instance_custom_data(index, Color(Time.get_ticks_msec() / 1000.0, duration,0,0))
 	_process_line(index, duration)
 
 func _process_line(index: int, duration: float) -> void:
-	var tween: Tween = create_tween()
-	await tween.tween_method(tween_line.bind(index), 1.0, 0.0, duration).finished
-	_close.erase(index)
+	await get_tree().create_timer(duration).timeout
 	_open.push_front(index)
-	
+
 func tween_line(opacity: float, index: int) -> void:
 	multimesh.set_instance_color(index, Color(multimesh.get_instance_color(index), opacity))
