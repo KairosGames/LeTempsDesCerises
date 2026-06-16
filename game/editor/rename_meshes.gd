91d6965aea2295876d613cdc7fc94ac9350af20b
D4r3N <enzo.suares@icloud.com>
2026-06-16 08:51:00 +0200

    editor script to rename meshes in scene

--- game/editor/rename_meshes.gd
+++ game/editor/rename_meshes.gd
@@ -7,19 +7,11 @@ func _run() -> void:
 	
 	var root: Node = selection[0]
 	
-	for child: Node in root.get_children():
+	var children: Array[Node] = root.get_children()
+	
+	for child: Node in children:
 		if child is not MeshInstance3D: continue
 		var mesh_instance: MeshInstance3D = child
 		var mesh: Mesh = mesh_instance.mesh
 		var name: String = mesh.resource_path.get_file().get_basename().to_pascal_case()
-		var group: Node = root.get_node_or_null(name)
-		if group is MeshInstance3D:
-			group.name += str(randi_range(0, 10000))
-			group = null
-		if not group: 
-			var new_group: Node3D = Node3D.new()
-			new_group.name = name
-			root.add_child(new_group)
-			group = new_group
-		
-		mesh_instance.reparent(group)
+		mesh_instance.name = name
