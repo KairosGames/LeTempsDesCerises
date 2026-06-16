2a618e66fa4ebbc5df04721edf69d189f3192d3d
D4r3N <enzo.suares@icloud.com>
2026-06-16 15:17:37 +0200

    fix is_pushing_canon on agent

--- game/prefabs/ai/behaviours/actions/push_canon.gd
+++ game/prefabs/ai/behaviours/actions/push_canon.gd
@@ -1,20 +1,18 @@
 @tool
 class_name PushCanon extends ActionLeaf
 
-var is_pushing: bool = false
-
 const INTERACTION_DISTANCE: float = 2
 
 func tick(actor: Node, _blackboard: Blackboard) -> int:
 	var agent: Agent = actor
-	if not is_pushing:
+	if not agent.is_pushing_canon:
 		if agent.global_position.distance_to(agent.canon_slot.global_position) > INTERACTION_DISTANCE:
 			return FAILURE
 		agent.navigation.disable()
 		agent.reparent(agent.canon_slot)
 		agent.collision_mask = 0
 		agent.transform = Transform3D.IDENTITY
-		is_pushing = true
+		agent.is_pushing_canon = true
 		return RUNNING
 	else: 
 		agent.transform = Transform3D.IDENTITY # FIXME
