11ddb6d53a988a994eb616cf80b31bc55b8e37e2
BENMANSOUR Ydris <ydris.benmansour.auditeur@lecnam.net>
2026-06-17 17:27:37 +0200

    fix

--- game/sounds/scripts/wwise_global.gd
+++ game/sounds/scripts/wwise_global.gd
@@ -176,7 +176,9 @@ func on_move_progress(progress : float):
 func on_reload_progress(progress):
 	if progress >= 0.9:
 		reload_gate = true
-		cannon_workers.filter(func(w): return is_instance_valid(w) and w).pick_random().post_event("Cannon_Incoming", 0)
+		var valid_workers: Array = cannon_workers.filter(func(w): return is_instance_valid(w) and w)
+		if valid_workers.size(): 
+			valid_workers.pick_random().post_event("Cannon_Incoming", 0)
 		find_closest(allies, barricade).post_event("Cannon_Incoming", 1)
 		find_random(allies).post_event("Cannon_Incoming", 2)
 
