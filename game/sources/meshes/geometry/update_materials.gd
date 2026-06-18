d6de6489df1d41df98b3388a3afe76339205c995
D4r3N <enzo.suares@icloud.com>
2026-06-18 13:44:00 +0200

    add log to material updading

--- game/sources/meshes/geometry/update_materials.gd
+++ game/sources/meshes/geometry/update_materials.gd
@@ -20,22 +20,22 @@ func _run() -> void:
 			push_error("Failed to load %s at %s" % [name, path])
 			continue
 		
-		_create_variant(mesh, name)
+		_create_variant(mesh, name, name)
 		
 		if MESH_VARIANTS.has(name):
 			for variant_name: String in MESH_VARIANTS[name]:
-				_create_variant(mesh, variant_name)
+				_create_variant(mesh, variant_name, name)
 	
-func _create_variant(original_mesh: ArrayMesh, name: String) -> void:
+func _create_variant(original_mesh: ArrayMesh, name: String, original_name: String) -> void:
 	var mesh: ArrayMesh = original_mesh.duplicate() # deep?
 	var output_path: String = OUTPUT_DIR + name + ".tres"
 	var existing: ArrayMesh = load(output_path)
 	for index: int in range(mesh.get_surface_count()):
 		var surface_name: String = mesh.surface_get_name(index)
-		if existing:
-			if existing.surface_get_name(index) == surface_name:
-				mesh.surface_set_material(index, existing.surface_get_material(index))
-			else:
-				mesh.surface_set_material(index, null)
+		if existing and existing.surface_get_name(index) == surface_name:
+			mesh.surface_set_material(index, existing.surface_get_material(index))
+		else:
+			mesh.surface_set_material(index, null)
 			push_warning("material ",surface_name," not available")
+	print("create %s variation of %s" % [name, original_name])
 	ResourceSaver.save(mesh, output_path)
