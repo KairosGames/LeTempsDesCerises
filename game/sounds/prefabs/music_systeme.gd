36d95a8166f67d4e1cfc3262f8c82c8dc5ec1cd3
Xelonn <124406589+Xelonn@users.noreply.github.com>
2026-06-19 18:09:13 +0200

    Sauf des mouchaaaaaaaaaaaaaaaaaaaaaaaaaaaaaards

--- game/sounds/prefabs/music_systeme.gd
+++ game/sounds/prefabs/music_systeme.gd
@@ -3,19 +3,23 @@ extends Node3D
 #@export var all_events : Array[AkEvent3D]
 
 func _ready() -> void:
-	GameManager.instance.all_states[2].stop_music_for_dialogue.connect(stop_all_music_event)
-	GameManager.instance.all_states[2].start_music_after_dialogue.connect(restart_music_after_dialogue)
+	#GameManager.instance.all_states[2].stop_music_for_dialogue.connect(stop_all_music_event)
+	#GameManager.instance.all_states[3].stop_music_for_ending.connect(stop_all_music_event)
+	#GameManager.instance.all_states[2].start_music_after_dialogue.connect(restart_music_after_dialogue)
+	pass
 
 func stop_all_music_event():
-	for event: AkEvent3D in get_children():
+	#for event: AkEvent3D in get_children():
 		#event.stop_event()
-		Wwise.post_event("Mu_Stop_All", self)
+		#Wwise.post_event("Mu_Stop_All", self)
+		pass
 
 
 func restart_music_after_dialogue():
 	print("On a set les states")
-	Wwise.set_state("Music_State", "Phase4")
-	Wwise.set_state("MusicVoicePlaying", "P4_1_Fight")
+	#Wwise.set_state("Music_State", "Phase4")
+	#Wwise.set_state("MusicVoicePlaying", "P4_1_Fight")
+	
 	#print(get_tree_string_pretty())
 	#for event : AkEvent3D in all_events:
 		#if event.Post_after_allies==true:
