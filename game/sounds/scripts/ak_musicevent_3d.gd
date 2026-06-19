eaa747f8915aa57698a014be29d77ee1f22f07da
Xelonn <124406589+Xelonn@users.noreply.github.com>
2026-06-19 18:09:13 +0200

    Sauf des mouchaaaaaaaaaaaaaaaaaaaaaaaaaaaaaards

--- game/sounds/scripts/ak_musicevent_3d.gd
+++ game/sounds/scripts/ak_musicevent_3d.gd
@@ -8,11 +8,16 @@ extends AkEvent3D
 @export var Enter_at: String = "None"
 @export var Post_event: bool = false
 @export var Post_after_allies: bool = false
+@export var Post_for_debug: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
 	GameManager.instance.all_states[2].start_music_after_dialogue.connect(post_after_allies)
+	GameManager.instance.all_states[3].music_debug_choice_scene.connect(post_event_debug)
+	
+	GameManager.instance.all_states[2].stop_music_for_dialogue.connect(stop_event_with_signal)
+	GameManager.instance.all_states[3].stop_music_for_ending.connect(stop_event_with_signal)
 	
 	Wwise.set_switch("Voice_Sel", Voice_sel, self)
 	DistScale.set_value(self, Distance_scale)
@@ -20,16 +25,22 @@ func _ready() -> void:
 		post_event()
 	IsPlay.set_value(self, Is_playing)
 
-
-
 func post_after_allies():
-	print("Func Post after allies")
+	#print("Func Post after allies")
 	if Post_after_allies == true:
-		print("Normalement ça posts")
+		#print("Normalement ça posts")
 		Wwise.set_switch("Voice_Sel", Voice_sel, self)
-		Is_playing = 100.0
-		IsPlay.set_value(self, Is_playing)
+		#Is_playing = 100.0
+		#IsPlay.set_value(self, Is_playing)
 		post_event()
+	pass
+
+func post_event_debug():
+	if Post_for_debug == true:
+		Wwise.set_state("MusicVoicePlaying","P4_2_End")
+		Wwise.set_switch("Voice_Sel", Voice_sel, self)
+		post_event()
+	pass
 
 func _on_music_sync_user_cue(data: Dictionary) -> void:
 	
@@ -40,5 +51,6 @@ func _on_music_sync_user_cue(data: Dictionary) -> void:
 			Is_playing = 100.0
 			IsPlay.set_value(self, Is_playing)
 			#print("j'ai trouvé : ", user_cue_name)
-			
-			
+
+func stop_event_with_signal():
+	stop_event()
