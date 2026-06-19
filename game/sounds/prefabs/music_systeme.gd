39e493d559f346dee4123e2ce25f976e7cc80f1c
Xelonn <124406589+Xelonn@users.noreply.github.com>
2026-06-19 13:04:27 +0200

    WIP Systeme Music

--- game/sounds/prefabs/music_systeme.gd
+++ game/sounds/prefabs/music_systeme.gd
@@ -1,18 +1,23 @@
 extends Node3D
 
+#@export var all_events : Array[AkEvent3D]
+
 func _ready() -> void:
 	GameManager.instance.all_states[2].stop_music_for_dialogue.connect(stop_all_music_event)
 	GameManager.instance.all_states[2].start_music_after_dialogue.connect(restart_music_after_dialogue)
 
 func stop_all_music_event():
 	for event: AkEvent3D in get_children():
-		event.stop_event()
+		#event.stop_event()
+		Wwise.post_event("Mu_Stop_All", self)
 
 
 func restart_music_after_dialogue():
+	print("On a set les states")
 	Wwise.set_state("Music_State", "Phase4")
 	Wwise.set_state("MusicVoicePlaying", "P4_1_Fight")
-	for event: AkEvent3D in get_children():
-		if event.Post_after_allies==true:
-			event.post_event()
-			print("SAUFDESMOUCHAAAAARDS")
+	#print(get_tree_string_pretty())
+	#for event : AkEvent3D in all_events:
+		#if event.Post_after_allies==true:
+			#event.post_event()
+			#print("SAUFDESMOUCHAAAAARDS")
