5774cd0c9740dc259de27bf5bc55c4766b7438f4
Xelonn <124406589+Xelonn@users.noreply.github.com>
2026-06-20 15:39:49 +0200

    Les chemiiiiiiiiiiiiiins (V1 du systeme de musique T-T)

--- game/prefabs/game_states/game_state_01.gd
+++ game/prefabs/game_states/game_state_01.gd
@@ -238,9 +238,6 @@ func go_to_cover_from_cannon() -> void:
 
 func launch_first_cannon_shoot() -> void:
 	game_ready_cannon_shoot.emit()
-	
-	Wwise.set_state("MusicVoicePlaying","P2_2_CanonShoot") ############# MUSIC
-	
 	await wait_signal(game_manager.first_barricade.state_changed)
 	for agent: Agent in game_manager.cannon.workers:
 		if not agent or not is_instance_valid(agent): continue
@@ -249,6 +246,7 @@ func launch_first_cannon_shoot() -> void:
 	take_player_move_control(false)
 	take_player_view_control(false)
 	player.can_play = true
+	Wwise.set_state("MusicVoicePlaying","P2_2_CannonShoot") ############# MUSIC
 	ui_manager.launch_letter_box(false)
 	await wait(ui_manager.time_to_open_letter_box)
 
