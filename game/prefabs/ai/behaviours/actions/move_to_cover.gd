04b8cb6f52e799fa6fbb412d8109c8ceda8decc3
D4r3N <enzo.suares@icloud.com>
2026-06-22 05:03:51 +0200

    Wait for cover rotation before move success

--- game/prefabs/ai/behaviours/actions/move_to_cover.gd
+++ game/prefabs/ai/behaviours/actions/move_to_cover.gd
@@ -3,6 +3,9 @@ class_name MoveToCover extends MoveAction
 
 @export var stop_distance: float = 0.5
 
+var _is_rotating_to_cover: bool = false
+var _rotation_finished: bool = false
+
 func pre_condition(actor: Node, _blackboard: Blackboard) -> bool:
 	var agent: Agent = actor
 	return agent.cover != null
@@ -11,6 +14,28 @@ func get_stop_distance() -> float: return stop_distance
 func get_destination(actor: Node, _blackboard: Blackboard) -> Vector3:
 	return (actor as Agent).cover.global_position
 
+func tick(actor: Node, blackboard: Blackboard) -> int:
+	var agent: Agent = actor
+
+	if _is_rotating_to_cover:
+		if _rotation_finished:
+			_is_rotating_to_cover = false
+			_rotation_finished = false
+			return SUCCESS
+		return RUNNING
+
+	return super(actor, blackboard)
+
 func on_success(actor: Node, _blackboard: Blackboard) -> void:
 	var agent: Agent = actor
-	agent.rotating_to(agent.cover.global_rotation.y, 0.5)
+	_is_rotating_to_cover = true
+	_rotation_finished = false
+	agent.rotating_to(agent.cover.global_rotation.y, 0.5).finished.connect(_on_rotation_finished, CONNECT_ONE_SHOT)
+
+func interrupt(actor: Node, blackboard: Blackboard) -> void:
+	_is_rotating_to_cover = false
+	_rotation_finished = false
+	super(actor, blackboard)
+
+func _on_rotation_finished() -> void:
+	_rotation_finished = true
