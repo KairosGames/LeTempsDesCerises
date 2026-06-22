7c3b12e045939273ad7d862d15a04bbcb70f492d
D4r3N <enzo.suares@icloud.com>
2026-06-22 13:18:37 +0200

    Revert "Avoid repeated move action start side effects"
    This reverts commit c31b5a4f8f30f2d2d43d6dfb49481ec5e728c022.

--- game/prefabs/ai/behaviours/actions/move_action.gd
+++ game/prefabs/ai/behaviours/actions/move_action.gd
@@ -5,30 +5,22 @@ class_name MoveAction extends ActionLeaf
 @abstract func get_destination(actor: Node, blackboard: Blackboard) -> Vector3
 @abstract func get_stop_distance() -> float
 
-var _is_move_active: bool = false
-
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	var agent: Agent = actor
 
-	if not pre_condition(actor, blackboard):
-		_stop_move(agent)
-		return FAILURE
+	if not pre_condition(actor, blackboard): return FAILURE
 
 	var destination: Vector3 = get_destination(actor, blackboard)
 	var stop_distance: float = get_stop_distance()
 
-	if not agent.can_move:
-		_stop_move(agent)
-		return FAILURE
+	if not agent.can_move: return FAILURE
 
 	if agent.navigation.target_position.distance_to(destination) > stop_distance: # handle moving target
 		agent.navigation.move_to(destination, stop_distance)
-		if not _is_move_active:
-			_is_move_active = true
-			agent.on_start_moving()
-			on_start(actor, blackboard)
+		agent.on_start_moving()
+		on_start(actor, blackboard)
 	if agent.navigation.is_navigation_finished():
-		_stop_move(agent)
+		agent.on_stop_moving()
 		var is_target_reached: bool = agent.navigation.is_target_reached()
 		if is_target_reached:
 			on_success(actor, blackboard)
@@ -40,12 +32,8 @@ func tick(actor: Node, blackboard: Blackboard) -> int:
 		return RUNNING
 
 func interrupt(actor: Node, _blackboard: Blackboard) -> void:
-	_stop_move(actor as Agent)
-
-func _stop_move(agent: Agent) -> void:
-	if _is_move_active:
-		_is_move_active = false
-		agent.on_stop_moving()
+	var agent: Agent = actor
+	agent.on_stop_moving()
 	agent.navigation.stop()
 
 func pre_condition(_actor: Node, _blackboard: Blackboard) -> bool: return true
