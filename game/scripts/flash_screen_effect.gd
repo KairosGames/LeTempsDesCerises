668d329deceae5d31b43e371bba330bf7b8146e2
BENMANSOUR Ydris <ydris.benmansour.auditeur@lecnam.net>
2026-06-24 11:03:45 +0200

    delete-input-debug

--- game/scripts/flash_screen_effect.gd
+++ game/scripts/flash_screen_effect.gd
@@ -2,10 +2,6 @@ class_name FlashScreenEffect extends ColorRect
 
 var twn: Tween
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("choice_surrender"):
-		play_flash_effect()
-
 func play_flash_effect() -> void:
 	color.a = 0.5
 	await get_tree().create_timer(0.025).timeout
