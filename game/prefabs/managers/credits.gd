41b41953c027a1919bcfaadd54743220f738f984
Vincent Barthélémi <barthelemivincent@gmail.com>
2026-06-25 00:00:23 +0200

    correct-subtitles-and-revamp-credits

--- game/prefabs/managers/credits.gd
+++ game/prefabs/managers/credits.gd
@@ -16,10 +16,12 @@ func start_credits():
 	text_box.show()
 	await get_tree().create_timer(5).timeout
 	allow_credits = true
-	await get_tree().create_timer(35).timeout
+	await get_tree().create_timer(40).timeout
+	self.hide()
 	get_tree().quit()
 
 func fade_in():
+	self.show()
 	var tween = get_tree().create_tween()
 	tween.tween_property(text_box, "modulate:a", 1, 3)
 	tween.play()
